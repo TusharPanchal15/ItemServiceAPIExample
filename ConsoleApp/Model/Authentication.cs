5ba7c4720c9dc8bef5bae83b7dc55b4fff7fd21d
Tusharkumar R Panchal <tpanchal@farahexperiences.com>
2022-03-31 21:11:58 +0530

    Add project files.

---
+++ ConsoleApp/Model/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp.Model
+{
+    public class Authentication
+    {
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ ConsoleApp/Model/ItemRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Model
+{
+    public class ItemRequest
+    {
+        public string ItemName { get; set; }
+
+        public string TemplateID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+}
